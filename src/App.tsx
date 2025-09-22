4678454a138b9bd9fc8bbd44ac72af13adbf7412
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-09-22 08:05:06 -0400

    Creación de un custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 import './App.css'
-import { Button } from './components'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const countMore=()=>{
-    setCount((count) => count + 1)
+  const [data, setData] = useState([])
+  const [loading,setLoading]=useState(false)
+  const [error,setError]=useState("")
+
+  const fetchData=async ()=>{
+    try {
+        setLoading(true)
+        const response=await fetch('https://api.example.com/data')
+        if (!response.ok){
+          throw new Error('Error al obtener los datos')
+        }
+        const jsconData=await response.json()
+        setData(jsconData)
+        
+    } catch (err) {
+        setError(err as string)
+      
+    }
+    finally{
+      setLoading(false)
+    }
   }
 
-  return (
-    <>
-      
-        <Button label={`count is ${count}`} parentMethod={countMore}/>
-       
-        
-    </>
+  // Comunicarnos al endpoint-entidad externa al componente
+  // Operaciones Async
+  //Parámetros de entrada
+
+  // Maneja el ciclo de vida de un componente
+  useEffect(()=>{
+    fetchData()
+  },[])
+  // useEffect(()=>{
+  //   // 1. Se va ejecutar cuando se  monta el componente
+  //   // 2. Cada vez que se modifique uno de los valores del state que está dentro del arreglo de dependencia
+
+  //   return ()=>{
+
+  //   }
+  // },[])
+  if (loading){
+    return (
+      <div>Cargando...</div>
+    )
+  }
+  if (error){
+    return (
+        <div>UPS! Hay un error: {error}</div>
+    )
+  }
+  return(
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
